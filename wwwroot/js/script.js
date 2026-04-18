d64d34f7386636fa2f68948d4ca2ddc64de45000
GermanTsymidanov <german151207@gmail.com>
2026-04-18 15:29:22 +1000

    Add files via upload

---
+++ wwwroot/js/script.js
@@ -0,0 +1,29 @@
+﻿const apiKey = "ccdefeff";
+
+document.getElementById("searchBtn").onclick = function () {
+
+    let movie = document.getElementById("movieInput").value;
+
+    fetch("https://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movie)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            if (data.Response === "False") {
+                document.getElementById("result").innerHTML = "Фильм не найден";
+                return;
+            }
+
+            document.getElementById("result").innerHTML =
+                "<h2>" + data.Title + "</h2>" +
+                "<p>Год: " + data.Year + "</p>" +
+                "<p>Рейтинг: " + data.imdbRating + "</p>" +
+                "<p>Режиссёр: " + data.Director + "</p>" +
+                "<p>Актёры: " + data.Actors + "</p>" +
+                "<p>Описание: " + data.Plot + "</p>";
+        })
+        .catch(function () {
+            document.getElementById("result").innerHTML = "Ошибка загрузки";
+        });
+};
